4bb0d50ba456fc51fcf7bbe332402de98df52c36
Emilio Ochoa <emilioaor@gmail.com>
2018-07-10 23:18:16 -0400

    Cambiando precios del formulario de contacto a dolares

--- resources/assets/js/components/FormContact.vue
+++ resources/assets/js/components/FormContact.vue
@@ -72,10 +72,10 @@
                         class="form-control"
                         >
                             <option value="0">¿Cuanto tenias pensado invertir?</option>
-                            <option>1.000.000 - 3.000.000 VEF</option>
-                            <option>3.000.001 - 6.000.000 VEF</option>
-                            <option>6.000.001 - 9.000.000 VEF</option>
-                            <option>9.000.001 o mas VEF</option>
+                            <option>50$ - 100$</option>
+                            <option>101$ - 150$</option>
+                            <option>151$ - 200$</option>
+                            <option>201$ o mas</option>
                     </select>
                     <p class="text-danger error" v-if="formContact.send && errors.firstByRule('amount', 'required')">
                         Campo requerido
